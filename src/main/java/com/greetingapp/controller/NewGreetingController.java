a48fc30efde68a8b67c092d9f58735794a666a18
ChandradipMoonlight <chandradipshivankar@gmail.com>
2021-08-09 22:53:18 +0530

    Getting: greeting messages With Hello, fristName lastName

--- src/main/java/com/greetingapp/controller/NewGreetingController.java
+++ src/main/java/com/greetingapp/controller/NewGreetingController.java
@@ -6,10 +6,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/greeting")
+@RequestMapping("/get")
 public class NewGreetingController {
 
     /**
@@ -22,8 +23,23 @@ public class NewGreetingController {
      * Purpose: Ability to return message using GET method form the service class.
      * @return message.
      */
-    @GetMapping(value = "/getMessage")
+    @GetMapping(value = "/message")
     public ResponseEntity<String> getMessage() {
         return new ResponseEntity<>(greetingService.getMessage(), HttpStatus.OK);
+    }
+
+    /**
+     * Purpose: Ability to return the Greeting message using service layer with
+     *          1. User First Name and Last Name or
+     * 			2. With just First Name or Last Name based on User attributes provides or
+     * 			3. Just Hello World.
+     * @param firstName;
+     * @param lastName;
+     * @return Hello firstName lastName! or firstName lastName or Hello World! etc.
+     */
+    @GetMapping(value = {"greeting"})
+    public ResponseEntity<String> getGreetingMessage(@RequestParam (value = "firstName", defaultValue = "World") String firstName,
+                                                     @RequestParam (value = "lastName", defaultValue = "") String lastName) {
+        return new ResponseEntity<>(greetingService.getGreetingMessage(firstName, lastName), HttpStatus.OK);
     }
 }
--- src/main/java/com/greetingapp/service/GreetingService.java
+++ src/main/java/com/greetingapp/service/GreetingService.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class GreetingService {
+    private final String template = "Hello, %s %s!";
 
     /**
      * this method is used to return string message to the controller.
@@ -11,5 +12,9 @@ public class GreetingService {
      */
     public String getMessage() {
         return "Hello World";
+    }
+
+    public String getGreetingMessage(String firstName, String lastName) {
+        return String.format(template, firstName, lastName);
     }
 }
